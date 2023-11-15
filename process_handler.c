e9bce549015141819536a9dea213c962d4078404
Yassine-SE <ytarkaoui@gmail.com>
2023-11-15 12:28:35 +0100

    v2.1

--- process_handler.c
+++ process_handler.c
@@ -29,7 +29,7 @@ void process_handler(char **token_array, char **env)
 		}
 		else
 		{
-			printf("hsh: %d: %s: not found\n", strlen(command), command);
+			printf("hsh: %u: %s: not found\n", strlen(command), command);
 		}
 		exit(EXIT_SUCCESS);
 	}
